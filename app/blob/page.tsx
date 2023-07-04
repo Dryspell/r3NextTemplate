0b00eeb67bc529bad542584e5316cca2c3786d1e
Dryspell <Nhwinslow@gmail.com>
2023-07-04 10:29:38 -0500

    init and move to ts

--- app/blob/page.tsx
+++ app/blob/page.tsx
@@ -2,12 +2,12 @@
 
 import dynamic from 'next/dynamic'
 
-const Blob = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Blob), { ssr: false })
-const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
+const Blob = dynamic(() => import('src/components/canvas/Examples').then((mod) => mod.Blob), { ssr: false })
+const View = dynamic(() => import('src/components/canvas/View').then((mod) => mod.View), {
   ssr: false,
   loading: () => (
-    <div className='flex h-96 w-full flex-col items-center justify-center'>
-      <svg className='-ml-1 mr-3 h-5 w-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
+    <div className='flex flex-col items-center justify-center w-full h-96'>
+      <svg className='w-5 h-5 mr-3 -ml-1 text-black animate-spin' fill='none' viewBox='0 0 24 24'>
         <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
         <path
           className='opacity-75'
@@ -18,20 +18,20 @@ const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.
     </div>
   ),
 })
-const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
+const Common = dynamic(() => import('src/components/canvas/View').then((mod) => mod.Common), { ssr: false })
 
 export default function Page() {
   return (
     <>
-      <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
-        <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
+      <div className='flex flex-col flex-wrap items-center w-full mx-auto md:flex-row lg:w-4/5'>
+        <div className='flex flex-col items-start justify-center w-full p-12 text-center md:w-2/5 md:text-left'>
           <p className='w-full uppercase'>Next + React Three Fiber</p>
           <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
           <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
         </div>
       </div>
 
-      <View className='absolute top-0 flex h-screen w-full flex-col items-center justify-center'>
+      <View className='absolute top-0 flex flex-col items-center justify-center w-full h-screen'>
         <Blob />
         <Common />
       </View>
--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -2,9 +2,9 @@
 
 import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
 import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
-import { Three } from '@/helpers/components/Three'
+import { Three } from 'src/helpers/components/Three'
 
-export const Common = ({ color }) => (
+export const Common = ({ color }: { color?: string }) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
     <ambientLight intensity={0.5} />
@@ -14,7 +14,7 @@ export const Common = ({ color }) => (
   </Suspense>
 )
 
-const View = forwardRef(({ children, orbit, ...props }, ref) => {
+const View = forwardRef(({ children, orbit, ...props }: any, ref) => {
   const localRef = useRef(null)
   useImperativeHandle(ref, () => localRef.current)
 
